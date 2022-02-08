c24cd93d9308c0a1247715673e32250bfc768e2e
suginami2357 <suginami2357@gmail.com>
2022-02-08 20:09:30 +0900

    AutoMigrate 追加

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"ReserveForm/commons/injections"
 	"ReserveForm/commons/servers"
+	"ReserveForm/models/contents"
+	"ReserveForm/models/reserves"
+	"ReserveForm/models/users"
+	"ReserveForm/repositories/postgreses"
 )
 
 func main() {
-	// gormDB := postgreses.Open();
-	// gormDB.AutoMigrate(&users.User{}, &reserves.Reserve{}, &contents.Content{})
+	gormDB := postgreses.Open()
+	gormDB.AutoMigrate(&users.User{}, &reserves.Reserve{}, &contents.Content{})
 	// gormDB.Create(&contents.Content{Name: "テスト1"})
 	// gormDB.Create(&contents.Content{Name: "テスト2"})
 	// gormDB.Create(&contents.Content{Name: "テスト3"})
