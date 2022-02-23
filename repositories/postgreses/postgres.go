b98d57e145f10bcf6201c42a4ce76b1e2463a919
suginami2357 <suginami2357@gmail.com>
2022-02-24 07:31:22 +0900

    db

--- repositories/postgreses/postgres.go
+++ repositories/postgreses/postgres.go
@@ -25,7 +25,8 @@ func Open() *gorm.DB {
 	// }), &gorm.Config{})
 
 	connection, _ := pq.ParseURL(os.Getenv("DATABASE_URL"))
-	connection += " host=postgres user=postgres password=password dbname=postgres sslmode=disable TimeZone=Asia/Shanghai"
+	// connection += " host=postgres user=postgres password=password dbname=postgres sslmode=disable"
+	connection += "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
 	sqlDB, _ := sql.Open("postgres", connection)
 	gormDB, _ := gorm.Open(postgres.New(postgres.Config{
 		Conn: sqlDB,
