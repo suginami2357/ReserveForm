5b3e923c40cdbc23782c846426eea7b864d32df9
suginami2357 <suginami2357@gmail.com>
2022-02-24 07:40:16 +0900

    db

--- repositories/postgreses/postgres.go
+++ repositories/postgreses/postgres.go
@@ -7,30 +7,30 @@ import (
 	"database/sql"
 	"os"
 
+	"github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-
-	"github.com/lib/pq"
-	_ "github.com/lib/pq"
 )
 
 //private
 func Open() *gorm.DB {
-	// mydb_dsn := os.Getenv("DATABASE_URL")
-	// connection, _ := pq.ParseURL(mydb_dsn)
-	// connection += " sslmode=require"
-	// sqlDB, _ := sql.Open("postgres", connection)
-	// gormDB, _ := gorm.Open(postgres.New(postgres.Config{
-	// 	Conn: sqlDB,
-	// }), &gorm.Config{})
 
-	connection, _ := pq.ParseURL(os.Getenv("DATABASE_URL"))
-	// connection += " host=postgres user=postgres password=password dbname=postgres sslmode=disable"
-	connection += "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	//HerokuでDBのセットアップをする場合、 DATABASE_URL という環境変数が設定される
+	url := os.Getenv("DATABASE_URL")
+	connection, _ := pq.ParseURL(url)
+	connection += " sslmode=require"
 	sqlDB, _ := sql.Open("postgres", connection)
 	gormDB, _ := gorm.Open(postgres.New(postgres.Config{
 		Conn: sqlDB,
 	}), &gorm.Config{})
+
+	// connection, _ := pq.ParseURL(os.Getenv("DATABASE_URL"))
+	// // connection += " host=postgres user=postgres password=password dbname=postgres sslmode=disable"
+	// connection += "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	// sqlDB, _ := sql.Open("postgres", connection)
+	// gormDB, _ := gorm.Open(postgres.New(postgres.Config{
+	// 	Conn: sqlDB,
+	// }), &gorm.Config{})
 
 	// dsn := "host=localhost user=postgres password=password dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	// gormDB, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})
