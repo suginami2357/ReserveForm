65d536785f5f9670208fb1487aa6d7a7f7bdcd6a
suginami2357 <suginami2357@gmail.com>
2022-02-26 13:24:54 +0900

    db

--- repositories/postgreses/postgres.go
+++ repositories/postgreses/postgres.go
@@ -13,11 +13,12 @@ import (
 //private
 func Open() *gorm.DB {
 	var dsn string
-	if os.Getenv("DATABASE_URL") == "" {
-		dsn = "host=localhost user=postgres password=password dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	} else {
-		dsn = os.Getenv("DATABASE_URL")
-	}
+	// if os.Getenv("DATABASE_URL") == "" {
+	// 	dsn = "host=localhost user=postgres password=password dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	// } else {
+	// 	dsn = os.Getenv("DATABASE_URL")
+	// }
+	dsn = os.Getenv("DATABASE_URL")
 	gormDB, _ := gorm.Open(postgres.Open(dsn))
 
 	// dsn, _ := pq.ParseURL(os.Getenv("DATABASE_URL"))
