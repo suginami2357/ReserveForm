2c9448fae570bbf70e485ca40f6bb1cde546db64
suginami2357 <suginami2357@gmail.com>
2022-02-26 14:51:49 +0900

    db

--- repositories/postgreses/postgres.go
+++ repositories/postgreses/postgres.go
@@ -4,21 +4,25 @@ import (
 
 	// "github.com/jinzhu/gorm"
 
-	"os"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 //private
 func Open() *gorm.DB {
-	var dsn string
 	// if os.Getenv("DATABASE_URL") == "" {
 	// 	dsn = "host=localhost user=postgres password=password dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	// } else {
-
+	// dsn = os.Getenv("DATABASE_URL") + "dbname=d7vogf43k1o4kp password=password port 5432"
 	// }
-	dsn = os.Getenv("DATABASE_URL") + "dbname=password password=password"
+
+	// dsn := "host=ec2-18-215-8-186.compute-1.amazonaws.com"
+	// dsn += "dbname=d7vogf43k1o4kp"
+	// dsn += "user=chyfofnwozqyc"
+	// dsn += "port=5432"
+	// dsn += "password=21e7e4cc5818d2a03ea783d235aa3131ef730e0d697196ede51be37fc411d05e"
+	// dsn += "sslmode=disable"
+	dsn := "postgres://chyfofnwozqyct:21e7e4cc5818d2a03ea783d235aa3131ef730e0d697196ede51be37fc411d05e@ec2-18-215-8-186.compute-1.amazonaws.com:5432/d7vogf43k1o4kp"
 	gormDB, _ := gorm.Open(postgres.Open(dsn))
 
 	// dsn, _ := pq.ParseURL(os.Getenv("DATABASE_URL"))
